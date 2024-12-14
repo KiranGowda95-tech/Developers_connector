6f99fdd416df16d82aabace83ea22ed2bfc33a6c
Kiran <kirankumardn74@gmail.com>
2024-12-14 18:02:49 +0530

    redux and alerts updated

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -4,21 +4,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 import "./App.css";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
-const App = () => {
-  return (
+const App = () => (
+  <Provider store={store}>
     <Router>
       <Fragment>
         <Navbar />
-        <Routes className='container'>
-          <Route exact path="/" element={<Landing />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
+        <div className="container">
+          <Alert />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
       </Fragment>
     </Router>
-  );
-};
+  </Provider>
+);
 
 export default App;
