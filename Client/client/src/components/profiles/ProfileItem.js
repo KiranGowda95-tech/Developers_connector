fda604b94f92b9c26171d6f21eda4720be22b545
Kiran <kirankumardn74@gmail.com>
2025-01-03 22:12:34 +0530

    files updated

--- Client/client/src/components/profiles/ProfileItem.js
+++ Client/client/src/components/profiles/ProfileItem.js
@@ -1,38 +1,45 @@
-import React,{Fragment,useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import Spinner from '../layout/Spinner'
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+//import { connect } from "react-redux";
+//import Spinner from "../layout/Spinner";
 
-const ProfileItem = ({profile:{
-    //user:{_id,name,avatar},
+const ProfileItem = ({
+  profile: {
+    user:{_id,name,avatar},
     status,
     company,
     location,
-    skills
-}}) => {
+    skills,
+  },
+}) => {
   return (
-    <div className='profile bg-light'>
-      {/* <img src={avatar} alt=''className=''/> */}
+    <div className="profile bg-light">
+       <img src={avatar} alt=''className=''/> 
       <div>
-        {/* <h2>{name}</h2> */}
-        <p>{status} {company && <span>at {company} </span>}</p>
-        <p className='my-1'>{location && <span>{location}</span>}</p>
-        {/* <Link to={`/profile/${_id}`} className='btn btn-primary'>View profile</Link> */}
+        <h2>{name}</h2>
+        <p>
+          {status} {company && <span>at {company} </span>}
+        </p>
+        <p className="my-1">{location && <span>{location}</span>}</p>
+        <Link to={`/profile/${_id}`} className="btn btn-primary">
+          View profile
+        </Link>
       </div>
       <ul>
-        {skills.slice(0,4).map((skill,index)=>(
-            <li key={index} className='text-primary'>
-                <i className='fas fa-check'/>{skill}
-            </li>
+        {skills.slice(0, 4).map((skill, index) => (
+          <li key={index} className="text-primary">
+            <i className="fas fa-check" />
+            {skill}
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 ProfileItem.propTypes = {
-profile:PropTypes.object.isRequired,
-}
+  profile: PropTypes.object.isRequired,
+};
 
-export default ProfileItem
+export default ProfileItem;
